b9aafc9fad45bed059496dbbcc7e766adc0a2909
Ronak8602 <ronakprajapati107@gmail.com>
2024-04-14 23:50:40 +0530

    Added key

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -128,7 +128,7 @@ function Table({}: Props) {
       <tbody>
         {results.map((v, i) => {
           return (
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-600">
+            <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-600">
               <td className="px-6 py-4 whitespace-nowrap text-left font-medium text-gray-900 dark:text-white">
                 <Link href={`/weatherDetails/${v.name}`}>{v.name}</Link>
               </td>
